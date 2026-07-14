cd2aae7a61bc2e9b6ddb7be9be566b64b11bfa4b
Jobin <josjobin@hotmail.com>
2026-07-14 10:25:16 +0200

    2026-06-24.dahlia

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2026-06-24.dahlia',
 });
 
 export async function GET(request: Request) {
